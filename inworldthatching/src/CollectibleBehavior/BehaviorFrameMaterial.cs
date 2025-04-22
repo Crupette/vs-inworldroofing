bcb13a90284fcbdefb3f6b1530ce4f7490c77dfa
Jon Santmyer <jon@jonsantmyer.com>
2025-04-22 10:30:40 -0400

    initial commit

---
+++ inworldthatching/src/CollectibleBehavior/BehaviorFrameMaterial.cs
@@ -0,0 +1,343 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
+
+namespace InWorldThatching;
+
+public class CollectibleBehaviorFrameMaterial : CollectibleBehavior
+{
+    public SkillItem[] ToolModes;
+    public AssetLocation[] FrameCodes;
+    public InWorldThatchingSystem inWorldThatchingSystem;
+
+    public const string SELECTED_FRAME_KEY = "inworldthatching.selectedframe";
+
+    public CollectibleBehaviorFrameMaterial(CollectibleObject collObj) : base(collObj)
+    {
+        
+    }
+
+    public override void OnLoaded(ICoreAPI api)
+    {
+        base.OnLoaded(api);
+        inWorldThatchingSystem = api.ModLoader.GetModSystem<InWorldThatchingSystem>();
+
+        FrameCodes = ObjectCacheUtil.GetOrCreate(api, "InWorldThatching.FrameCodes", () => {
+            return new[] {
+                AssetLocation.Create("thatchframe-straight-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-bottom-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-cornerinner-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-cornerouter-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-tip-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-ridge-north", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-halfleft-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-halfright-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-ridgeend-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-ridgehalfleft-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-ridgehalfright-east", InWorldThatchingSystem.MODID),
+                AssetLocation.Create("thatchframe-top-east", InWorldThatchingSystem.MODID),
+            };
+        });
+
+        if(api is not ICoreClientAPI capi) return;
+        ToolModes = ObjectCacheUtil.GetOrCreate(api, "InWorldThatching.ThatchFrameMaterialToolModes", () => {
+            List<SkillItem> modes = new() {
+                new(){
+                    Code = AssetLocation.Create("default", InWorldThatchingSystem.MODID),
+                    Name = Lang.Get("toolmode-thatchframematerial-placeonground"),
+                    TexturePremultipliedAlpha = false
+                },
+                new(){
+                    Code = AssetLocation.Create("makeframe", InWorldThatchingSystem.MODID),
+                    Name = Lang.Get("toolmode-thatchframematerial-placeframe"),
+                    TexturePremultipliedAlpha = false
+                }
+            };
+
+            modes[0].WithIcon(capi, capi.Gui.LoadSvgWithPadding(
+                AssetLocation.Create("textures/icons/cross.svg", InWorldThatchingSystem.MODID),
+                48, 48, 5, ColorUtil.WhiteArgb));
+            modes[1].WithIcon(capi, capi.Gui.LoadSvgWithPadding(
+                AssetLocation.Create("textures/icons/frame.svg", InWorldThatchingSystem.MODID),
+                48, 48, 5, ColorUtil.WhiteArgb));
+
+            return modes.ToArray();
+        });
+    }
+
+    public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling, ref EnumHandling handling)
+    {
+        IWorldAccessor world = byEntity?.World;
+        if(byEntity == null || world == null) return;
+
+        if(blockSel == null) return;
+
+        if(byEntity.Controls.ShiftKey) {
+            IPlayer player = (byEntity as EntityPlayer)?.Player;
+            if(player == null) return;
+
+            int toolMode = GetToolMode(slot, (byEntity as EntityPlayer).Player, blockSel);
+            if(toolMode == 0) return;
+
+            handling = EnumHandling.PreventSubsequent;
+            handHandling = EnumHandHandling.PreventDefault;
+
+            BlockPos placePos = blockSel.Position.AddCopy(blockSel.Face);
+            if(!byEntity.World.Claims.TryAccess(player, placePos, EnumBlockAccessFlags.BuildOrBreak)) {
+                slot.MarkDirty();
+                return;
+            }
+            
+            int selectedId = GetSelectedFrame(slot);
+
+            Block selectedBlock = world.BlockAccessor.GetBlock(FrameCodes[selectedId]);
+            if(selectedBlock == null)
+                throw new System.NullReferenceException($"Unable to find frame block with code {FrameCodes[selectedId]}");
+
+            if(selectedBlock is not BlockThatchFrame selectedFrame)
+                throw new System.NullReferenceException($"Cannot create frame block from non-frame type block {FrameCodes[selectedId]}");
+
+            switch(selectedFrame.OrientableBehavior) {
+                case EnumFrameOrientableBehavior.NONE: {
+                    if(!PlaceBlockNotOrientable(world, slot, player, blockSel, selectedFrame)) {
+                        return;
+                    }
+                } break;
+                case EnumFrameOrientableBehavior.NWORIENTABLE: {
+                    if(!PlaceBlockNWOrientable(world, slot, player, blockSel, selectedBlock)) {
+                        return;
+                    }
+                } break;
+                case EnumFrameOrientableBehavior.HORIENTABLE: {
+                    if(!PlaceBlockHOrientable(world, slot, player, blockSel, selectedBlock)) {
+                        return;
+                    }
+                } break;
+            }
+        }
+    }
+
+    public bool PlaceBlockNotOrientable(IWorldAccessor world, ItemSlot slot, IPlayer player, BlockSelection blockSel, BlockThatchFrame selectedFrame)
+    {
+        BlockPos placePos = blockSel.Position.AddCopy(blockSel.Face);
+        if(world.BlockAccessor.GetBlock(placePos).Replaceable < 6000) {
+            return false;
+        }
+
+        CraftingRecipeIngredient cost = selectedFrame.Cost;
+        if(slot.Itemstack.StackSize < cost.Quantity) {
+            (world.Api as ICoreClientAPI)?.TriggerIngameError(this, "notenough", Lang.Get("ingameerror-thatchframematerial-notenoughsticks", slot.Itemstack.StackSize, cost.Quantity));
+            return false;
+        }
+
+        slot.TakeOut(cost.Quantity);
+        world.BlockAccessor.SetBlock(selectedFrame.Id, placePos);
+
+        if(world.Side == EnumAppSide.Server) {
+            world.PlaySoundAt(
+                AssetLocation.Create("sounds/block/loosestick"), 
+                blockSel.Position,
+                -0.5);
+        }
+        return true;
+    }
+
+    public bool PlaceBlockHOrientable(IWorldAccessor world, ItemSlot slot, IPlayer player, BlockSelection blockSel, Block selectedBlock)
+    {
+        BlockPos placePos = blockSel.Position.AddCopy(blockSel.Face);
+        if(world.BlockAccessor.GetBlock(placePos).Replaceable < 6000) {
+            return false;
+        }
+
+        BlockFacing[] horVer = Block.SuggestedHVOrientation(player, blockSel);
+        AssetLocation orientedCode = selectedBlock.CodeWithVariant("horizontalorientation", horVer[0].Code);
+        Block orientedBlock = world.BlockAccessor.GetBlock(orientedCode);
+
+        if(orientedBlock == null)
+            throw new System.NullReferenceException($"Unable to find rotated frame with code {orientedCode}");
+
+        if (orientedBlock is not BlockThatchFrame frameBlock)
+            throw new System.NullReferenceException($"Unable to calculate cost for non-frame block {orientedCode}");
+
+        CraftingRecipeIngredient cost = frameBlock.Cost;
+        if(slot.Itemstack.StackSize < cost.Quantity) {
+            (world.Api as ICoreClientAPI)?.TriggerIngameError(this, "notenough", Lang.Get("Not enough sticks! Have {0}, need {1}", slot.Itemstack.StackSize, cost.Quantity));
+            return false;
+        }
+
+        slot.TakeOut(cost.Quantity);
+        world.BlockAccessor.SetBlock(orientedBlock.Id, placePos);
+
+        if(world.Side == EnumAppSide.Server) {
+            world.PlaySoundAt(
+                AssetLocation.Create("sounds/block/loosestick"), 
+                blockSel.Position,
+                -0.5);
+        }
+        return true;
+    }
+
+    public bool PlaceBlockNWOrientable(IWorldAccessor world, ItemSlot slot, IPlayer player, BlockSelection blockSel, Block selectedBlock)
+    {
+        BlockPos placePos = blockSel.Position.AddCopy(blockSel.Face);
+        if(world.BlockAccessor.GetBlock(placePos).Replaceable < 6000) {
+            return false;
+        }
+
+        BlockFacing[] horVer = Block.SuggestedHVOrientation(player, blockSel);
+        string orientCode = "north";
+        if(horVer[0].Index == 1 || horVer[0].Index == 3) orientCode = "east";
+        AssetLocation orientedCode = selectedBlock.CodeWithVariant("horizontalorientation", orientCode);
+
+        Block orientedBlock = world.BlockAccessor.GetBlock(orientedCode);
+
+        if(orientedBlock == null)
+            throw new System.NullReferenceException($"Unable to find rotated frame with code {orientedCode}");
+
+        if (orientedBlock is not BlockThatchFrame frameBlock)
+            throw new System.NullReferenceException($"Unable to calculate cost for non-frame block {orientedCode}");
+
+        CraftingRecipeIngredient cost = frameBlock.Cost;
+        if(slot.Itemstack.StackSize < cost.Quantity) {
+            (world.Api as ICoreClientAPI)?.TriggerIngameError(this, "notenough", Lang.Get("Not enough sticks! Have {0}, need {1}", slot.Itemstack.StackSize, cost.Quantity));
+            return false;
+        }
+
+        slot.TakeOut(cost.Quantity);
+        world.BlockAccessor.SetBlock(orientedBlock.Id, placePos);
+
+        if(world.Side == EnumAppSide.Server) {
+            world.PlaySoundAt(
+                AssetLocation.Create("sounds/block/loosestick"), 
+                blockSel.Position,
+                -0.5);
+        }
+        return true;
+    }
+
+    public override SkillItem[] GetToolModes(ItemSlot slot, IClientPlayer forPlayer, BlockSelection blockSel)
+    {
+        if(blockSel == null) return null;
+        return ToolModes;
+    }
+
+    public override int GetToolMode(ItemSlot slot, IPlayer byPlayer, BlockSelection blockSelection)
+    {
+        return slot.Itemstack.Attributes.GetInt("toolMode");
+    }
+
+    public override void SetToolMode(ItemSlot slot, IPlayer byPlayer, BlockSelection blockSelection, int toolMode)
+    {
+        slot.Itemstack.Attributes.SetInt("toolMode", toolMode);
+        if(toolMode != 1) {
+            if(toolMode == 0) {
+                slot.Itemstack.Attributes.RemoveAttribute("toolMode");
+            }
+            slot.Itemstack.Attributes.RemoveAttribute(SELECTED_FRAME_KEY);
+            return;
+        }
+        if (byPlayer == null || byPlayer.Entity.World.Side != EnumAppSide.Client) return;
+
+        BlockPos selPos = blockSelection.Position;
+        OpenDialog(byPlayer as IClientPlayer, selPos, slot);
+    }
+
+    public override WorldInteraction[] GetHeldInteractionHelp(ItemSlot inSlot, ref EnumHandling handling)
+    {
+        ICoreAPI api = inSlot.Inventory.Api;
+        WorldInteraction[] interactions = new WorldInteraction[] {
+            new() {
+                ActionLangCode = "heldhelp-thatchframetoolmode",
+                HotKeyCode = "toolmodeselect",
+                MouseButton = EnumMouseButton.None
+            }
+        };
+        int toolMode = GetToolMode(inSlot, null, null);
+        if(toolMode == 0) {
+            return interactions.Append(base.GetHeldInteractionHelp(inSlot, ref handling));
+        }
+        if(toolMode == 1) {
+            handling = EnumHandling.PreventDefault;
+            int selectedId = GetSelectedFrame(inSlot);
+            if (api.World.GetBlock(FrameCodes[selectedId]) is not BlockThatchFrame blockFrame)
+                throw new NullReferenceException($"Failed to retrieve frame block from code {FrameCodes[selectedId]}");
+
+            ItemStack frameStack = new ItemStack(blockFrame);
+            CraftingRecipeIngredient cost = blockFrame.Cost;
+            cost.Resolve(api.World, "CollectibleBehaviorFrameMaterial.GetHeldInteractionHelp");
+
+            return new WorldInteraction[] {
+                new () {
+                    ActionLangCode = Lang.Get("heldhelp-placethatchframe", blockFrame.GetHeldItemName(frameStack).ToLower()),
+                    HotKeyCode = "shift",
+                    MouseButton = EnumMouseButton.Right,
+                    Itemstacks = new ItemStack[] { cost.ResolvedItemstack }
+                }
+            };
+        }
+        return base.GetHeldInteractionHelp(inSlot, ref handling);
+    }
+
+    public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+    {
+        base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+        if(GetToolMode(inSlot, null, null) != 0) {
+            dsc.Append(Lang.Get("heldinfo-thatchframematerial-clearattributes"));
+        }
+    }
+
+#region Client
+    GuiDialog dialog;
+    public void OpenDialog(IClientPlayer byPlayer, BlockPos pos, ItemSlot slot) {
+        if(dialog != null && dialog.IsOpened()) return;
+        IClientWorldAccessor world = byPlayer.Entity.World as IClientWorldAccessor;
+
+        List<ItemStack> stacks = new();
+        foreach(var code in FrameCodes) {
+            BlockThatchFrame frameBlock = world.GetBlock(code) as BlockThatchFrame;
+            Block displayBlock = world.GetBlock(frameBlock.RecipeDisplay);
+            stacks.Add(new ItemStack(displayBlock));
+        }
+
+        ICoreClientAPI capi = world.Api as ICoreClientAPI;
+        dialog = new GuiDialogBlockEntityRecipeSelector(
+            Lang.Get("Select roof type"),
+            stacks.ToArray(),
+            (selectedIndex) => {
+                SetSelectedFrame(slot, selectedIndex);
+                inWorldThatchingSystem.SendSelectMessage(byPlayer, slot, selectedIndex);
+                slot.MarkDirty();
+            },
+            () => {
+                SetToolMode(slot, null, null, 0);
+                slot.MarkDirty();
+            },
+            pos,
+            capi
+        );
+        dialog.OnClosed += dialog.Dispose;
+        dialog.TryOpen();
+    }
+#endregion
+
+    public static int GetSelectedFrame(ItemSlot slot)
+    {
+        ItemStack stack = slot.Itemstack;
+        return stack.Attributes.GetInt(SELECTED_FRAME_KEY, 0);
+    }
+
+    public static void SetSelectedFrame(ItemSlot slot, int value)
+    {
+        ItemStack stack = slot.Itemstack;
+        stack.Attributes.SetInt(SELECTED_FRAME_KEY, value);
+        slot.Itemstack.Attributes.SetInt("toolMode", 1);
+    }
+}
---
+++ inworldthatching/src/InWorldThatchingSystem.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection.Metadata;
+using ProtoBuf;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace InWorldThatching;
+
+[ProtoContract]
+public class ThatchFrameSelectMessage 
+{
+    [ProtoMember(1)]
+    public int slotId;
+    [ProtoMember(2)]
+    public int selection;
+}
+
+public enum EnumFrameOrientableBehavior
+{
+    NONE,
+    NWORIENTABLE,
+    HORIENTABLE,
+}
+
+public class InWorldThatchingSystem : ModSystem
+{
+    public const string MODID = "inworldthatching";
+    public const string FRAME_SELECT_CHANNEL_NAME = MODID + ".thatchframeselect";
+
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterBlockClass("InWorldThatching.BlockThatchFrame", typeof(BlockThatchFrame));
+        api.RegisterBlockClass("InWorldThatching.BlockThatchWork", typeof(BlockThatchWork));
+        api.RegisterCollectibleBehaviorClass("InWorldThatching.CollectibleBehaviorFrameMaterial", typeof(CollectibleBehaviorFrameMaterial));
+
+        api.Network.RegisterChannel(FRAME_SELECT_CHANNEL_NAME)
+                   .RegisterMessageType(typeof(ThatchFrameSelectMessage));
+    }
+    #region Server
+    public ICoreServerAPI sapi;
+    public IServerNetworkChannel serverThatchFrameChannel;
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        sapi = api;
+
+        serverThatchFrameChannel = 
+            api.Network.GetChannel(FRAME_SELECT_CHANNEL_NAME)
+                       .SetMessageHandler<ThatchFrameSelectMessage>((fromPlayer, message) => 
+        {
+            ItemSlot slot = fromPlayer.InventoryManager.GetHotbarInventory()[message.slotId];
+            CollectibleBehaviorFrameMaterial.SetSelectedFrame(slot, message.selection);
+            slot.MarkDirty();
+        });
+                        
+    }
+    #endregion
+
+    #region Client
+
+    IClientNetworkChannel clientThatchFrameChannel;
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        clientThatchFrameChannel =
+            api.Network.GetChannel(FRAME_SELECT_CHANNEL_NAME);
+    }
+
+    public void SendSelectMessage(IClientPlayer player, ItemSlot slot, int selection)
+    {
+        ThatchFrameSelectMessage message = new() {
+            slotId = player.InventoryManager.GetHotbarInventory().GetSlotId(slot),
+            selection = selection
+        };
+        clientThatchFrameChannel.SendPacket(message);
+    }
+    #endregion
+}
